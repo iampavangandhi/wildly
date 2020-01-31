2ee1f6926d0eec66d27a07c02d803c4aba11fa83
Pavan Gandhi <pavangandhi100@gmail.com>
2020-01-31 18:15:54 +0530

    added discover.dart

--- lib/pages/discover.dart
+++ lib/pages/discover.dart
@@ -6,8 +6,24 @@ class Discover extends StatefulWidget {
 }
 
 class _DiscoverState extends State<Discover> {
+  final List<String> entries = <String>['A', 'B', 'C', 'D'];
+  final List<int> colorCodes = <int>[600, 500, 400, 300];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: ListView.builder(
+            padding: const EdgeInsets.all(7),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) =>
+                singleListItem(index)));
+  }
+
+  singleListItem(index) {
+    return Container(
+      height: 100,
+      margin: EdgeInsets.all(7.0),
+      color: Colors.teal[colorCodes[index]],
+      child: Center(child: Text('Entry ${entries[index]}')),
+    );
   }
 }
