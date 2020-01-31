3761cad0159354dda31894730b82c689b231fa71
Toseefkhan403 <toseefkhan403@gmail.com>
2020-01-31 18:12:49 +0530

    progress

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -8,6 +8,24 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(padding: EdgeInsets.only(top: 64.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: signUpGradients,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
+    child: ListView(
+    ),);
   }
 }
+
+const List<Color> signUpGradients = [
+  /* Color(0xFFFF9945),
+  Color(0xFFFc6076),*/
+  /*kShrinePink500,
+  kShrinePink600,
+  Colors.pinkAccent,*/
+  Colors.black,
+  Colors.black87
+];
