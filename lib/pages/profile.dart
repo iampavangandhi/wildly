83e16a63a0d0198f478affd03a1265eff9737580
pawanVerma1667 <pawanjbs5@gmail.com>
2020-02-01 00:36:59 +0530

    ok

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
+import 'animal_single_item_page.dart';
+
 class Profile extends StatefulWidget {
   @override
   _ProfileState createState() => _ProfileState();
@@ -9,9 +11,9 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
 
-  String displayName, email;
+  String displayName = "", email = "";
   String photoUrl = "";
-
+  List myAnimalsList = List();
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +21,10 @@ class _ProfileState extends State<Profile> {
     final _height = MediaQuery.of(context).size.height;
 
     return new Stack(children: <Widget>[
-      new Container(color: Colors.tealAccent.withOpacity(0.6),),
-      new Image.network(photoUrl, fit: BoxFit.fill,),
+      new Container(color: Colors.blueGrey.withOpacity(0.6),),
       new Container(
         decoration: BoxDecoration(
-          color:  Colors.tealAccent.withOpacity(0.6),
+          color:  Colors.blueGrey.withOpacity(0.6),
           borderRadius: BorderRadius.all(Radius.circular(50.0)),
         ),),
       new Scaffold(
@@ -32,18 +33,140 @@ class _ProfileState extends State<Profile> {
             child: new Column(
               children: <Widget>[
                 new SizedBox(height: _height/12,),
-                new CircleAvatar(radius:_width<_height? _width/4:_height/4,backgroundImage: NetworkImage(photoUrl),),
-                new SizedBox(height: _height/25.0,),
+                new CircleAvatar(radius:_width<_height? _width/8:_height/8,backgroundImage: NetworkImage(photoUrl),),
+                new SizedBox(height: 25.0,),
                 new Text('$displayName', style: new TextStyle(fontWeight: FontWeight.bold, fontSize: _width/15, color: Colors.white),),
-                new Padding(padding: new EdgeInsets.only(top: _height/30, left: _width/8, right: _width/8),
+                new Padding(padding: new EdgeInsets.only(top: 0.0, left: _width/8, right: _width/8),
                   child:new Text('$email',
                     style: new TextStyle(fontWeight: FontWeight.normal, fontSize: _width/25,color: Colors.white),textAlign: TextAlign.center,) ,),
+                Container(padding: EdgeInsets.all(10.0),child: new Text('My Contributions', style: new TextStyle(fontWeight: FontWeight.bold, fontFamily: 'DancingScript', color: Colors.white, fontSize: 28.0),)),
+                _horizontalList(myAnimalsList),
               ],
             ),
           )
       )
-    ],);
+    ]);
   }
+
+  _horizontalList(List storyList) => Container(
+    padding: EdgeInsets.all(12.0),
+    height: 280.0,
+    child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: storyList.length,
+        itemBuilder: (context, index) => _listItem(storyList[index])),
+  );
+
+  _listItem(story) {
+    return InkWell(
+      onTap: ()  {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => AnimalSingleItemPage()));
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                      height: 250.0,
+                      width: 200.0,
+                      child: Image.network(
+                        story['image'],
+                        fit: BoxFit.cover,
+                      )),
+                  Positioned(
+                    left: 0.0,
+                    bottom: 0.0,
+                    width: 200.0,
+                    height: 60.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: [
+                                Colors.black,
+                                Colors.black.withOpacity(0.01),
+                              ])),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0.0,
+                    right: 8.0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.9),
+                      child: Chip(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.circular(10.0)),
+                          backgroundColor: Colors.white,
+                          labelPadding: EdgeInsets.symmetric(
+                              horizontal: 4.0, vertical: 0.0),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 0.0, horizontal: 3.0),
+                          label: Text(
+                            story['type'],
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black,
+                                fontSize: 11.0),
+                          )),
+                    ),
+                  ),
+                  Positioned(
+                    left: 10.0,
+                    bottom: 10.0,
+                    right: 10.0,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+
+                            Container(
+                              width: 160.0,
+                              child: Text(
+                                '${story['name']}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 18.0),
+                              ),
+                            ),
+                            Container(
+                              width: 160.0,
+                              child: Text(
+                                '${descriptionStringShort(story['cause'])}',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 13.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  descriptionStringShort(String description) => description.length > 22
+      ? description.substring(0, 22) + '...'
+      : description;
 
   @override
   void initState() {
@@ -55,8 +178,11 @@ class _ProfileState extends State<Profile> {
 
     FirebaseUser user = await FirebaseAuth.instance.currentUser();
     DataSnapshot snapshot = await FirebaseDatabase.instance.reference().child('user_account_settings').child(user.uid).once();
-    setState(() {
+    DataSnapshot snapshot1 =
+    await FirebaseDatabase.instance.reference().child('user_animals').child(user.uid).once();
 
+    setState(() {
+      myAnimalsList = (snapshot1.value as Map<dynamic, dynamic>).values.toList();
       displayName = snapshot.value['displayName'];
       photoUrl = snapshot.value['photoUrl'];
       email = snapshot.value['email'];
@@ -64,7 +190,7 @@ class _ProfileState extends State<Profile> {
       if(photoUrl == null){
         photoUrl = "https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png";
       }
-      if (displayName == null){
+      if (displayName == ""){
         List<String> f = email.split('@');
         displayName = f[0];
       }
